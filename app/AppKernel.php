60380ed2507e04b93b7db29c758fde7a0bcae970
Jose Ignacio <joserobleda@gmail.com>
2015-09-26 14:54:38 +0200

    doctrine migrations

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,9 +15,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new SimpleBus\SymfonyBridge\SimpleBusCommandBusBundle(),
             new AppBundle\AppBundle(),
-            new SimpleBus\SymfonyBridge\SimpleBusCommandBusBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
